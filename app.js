0857b7606015da20ee97435256256f5b17b8c217
Samreen Fatima <irfankhans786787789@gmail.com>
2024-09-12 21:03:03 +0500

    completed

---
+++ app.js
@@ -0,0 +1,27 @@
+var num1 = 1;
+var num2 = 3;
+var totle = num1 + num2
+console.log(totle)
+
+
+var num1 = 1;
+var num2 = 3;
+console.log(num1 + num2)
+
+
+var num1 = 2 ;
+var totle = num1++ + --num1 + ++num1-num1--;
+                
+
+console.log(totle)
+
+
+var num = 1 + (2 * 2);
+console.log(num)
+
+var firstName = "sara "
+var lastName = "fatima"
+var fullName = firstName + lastName;
+document.write("hello " + fullName)
+
+console.log(2)
